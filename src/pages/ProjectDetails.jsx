04b9badd06f1a241a58c305ca225338f8d7adc87
anra15 <anra1507@gmail.com>
2025-05-05 15:03:02 -0600

    format_new

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -13,14 +13,14 @@ function ProjectDetails() {
     return (
         <div className="projects-display">
             <div className="content-container">
-                <h1 className="project-title">{project.name}</h1>
+                <h4 className="project-title">{project.name}</h4>
                 <img src={project.image} alt={project.name} className="project-image"/>
-                <p className="globalgoal-title">{project.globalgoal}</p>
+                <h1 className="globalgoal-title">{project.globalgoal}</h1>
                 <p className="project-description">{project.description}</p>
-                <p className="project-description">Type of project: {project.tags.type}</p>
-                <p className="project-description">Frontend sugestions for the project: {project.tags.frontend}</p>
-                <p className="project-description">Backend sugestions for the project: {project.tags.backend}</p>
-                <p className="project-description">Database sugestions for the project: {project.tags.database}</p>
+                <p className="project-description-sub">Type of project:</p><p className="project-description">{project.tags.type}</p>
+                <p className="project-description-sub">Frontend sugestions for the project:</p><p className="project-description"> {project.tags.frontend}</p>
+                <p className="project-description-sub">Backend sugestions for the project:</p><p className="project-description"> {project.tags.backend}</p>
+                <p className="project-description-sub">Database sugestions for the project:</p><p className="project-description"> {project.tags.database}</p>
             </div>
         </div>
     );
