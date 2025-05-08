022ad030cc42e73047001038437b880e643ac1a6
anra15 <anra1507@gmail.com>
2025-05-07 20:00:03 -0600

    fixed

--- src/pages/FindAProject.jsx
+++ src/pages/FindAProject.jsx
@@ -194,21 +194,24 @@ function FindAProject() {
                   </Link>
                 ))}
               </div>
-          <>
-            <div className="recommended-projects">
-              <h2>Recommended Projects:</h2>
-              {recommendedProject.map((project) => (
-                <Link to={`/projectdetails/${project.id}`} key={project.id} className="project-card">
-                  <img src={project.image} alt={project.name} className="project-image" />
-                  <div className="project-info">
-                    <h3 className="project-title">{project.name}</h3>
-                    <h4 className='globalgoal'>{project.globalgoal}</h4>
-                    <p className="project-description">{project.description}</p>
-                  </div>
-                </Link>
-              ))}
+              <>
+                <div className="recommended-projects">
+                  <h2>Recommended Projects:</h2>
+                  {recommendedProject.map((project) => (
+                    <Link to={`/projectdetails/${project.id}`} key={project.id} className="project-card">
+                      <img src={project.image} alt={project.name} className="project-image" />
+                      <div className="project-info">
+                        <h3 className="project-title">{project.name}</h3>
+                        <h4 className='globalgoal'>{project.globalgoal}</h4>
+                        <p className="project-description">{project.description}</p>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+
+              </>
             </div>
-          </>
+          </div>
         )}
       </div>
     </div>
