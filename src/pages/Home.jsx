38d7ff075014169d1da869add6a8b6a70aedcfd9
anra15 <anra1507@gmail.com>
2025-05-07 22:11:17 -0600

    test_y

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
         <p className="explanation-text">Here, you'll find guidance to find the project that better fits to your needs and the world's.</p>
       </div>
       <div className="button-container">
-        <button className="button-style"><Link to="findaproject">Find your project!</Link></button>
+        <button className="button-style"><Link to="/findaproject">Find your project!</Link></button>
       </div>
       <div className="sta-container"  style={{ color: 'black' }}>
         <h1 className="sta-title">
